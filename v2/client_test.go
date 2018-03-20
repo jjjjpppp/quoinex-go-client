06f4d58460cfe9ea95a5bb3eb6eacbf724001b5c
Jun Ishioka <jjjjpppp25@gmail.com>
2018-03-20 17:45:52 +0900

    changed to use table driven test

--- v2/client_test.go
+++ v2/client_test.go
@@ -3,182 +3,190 @@ package quoinex
 import (
 	"context"
 	"fmt"
+	"github.com/google/go-cmp/cmp"
+	"github.com/jjjjpppp/quoinex-go-client/v2/models"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 	"time"
 )
 
-func TestNewClientApiTokenError(t *testing.T) {
-	if _, e := NewClient("", "secret", nil); e == nil {
-		t.Error("err should be returned")
+func TestNewClient(t *testing.T) {
+	type Param struct {
+		apiToken  string
+		apiSecret string
+		logger    *log.Logger
+	}
+	type Expect struct {
+		client *Client
+		err    error
+	}
+	cases := []struct {
+		param  Param
+		expect Expect
+	}{
+		// test case 1
+		{
+			param:  Param{apiToken: "", apiSecret: "secret", logger: nil},
+			expect: Expect{client: nil, err: fmt.Errorf("apiTokenID is not set")},
+		},
+		// test case 2
+		{
+			param:  Param{apiToken: "apiToken", apiSecret: "", logger: nil},
+			expect: Expect{client: nil, err: fmt.Errorf("apiSecret is not set")},
+		},
+		// test case 3
+		{
+			param: Param{apiToken: "apiToken", apiSecret: "apiSecret", logger: nil},
+			expect: Expect{client: &Client{
+				ApiTokenID: "apiToken",
+				ApiSecret:  "apiSecret",
+				HTTPClient: &http.Client{Timeout: time.Duration(10) * time.Second},
+				Logger:     log.New(ioutil.Discard, "", log.LstdFlags),
+			}, err: nil},
+		},
+	}
+	for _, c := range cases {
+		client, e := NewClient(c.param.apiToken, c.param.apiSecret, c.param.logger)
+		if client == nil && e.Error() != c.expect.err.Error() {
+			t.Errorf("Worng err. test set is %+v", c)
+		}
+		if client == nil {
+			t.Logf("client is nil. skip this case.: test set: %+v", c)
+			continue
+		}
+		if client.ApiTokenID != c.expect.client.ApiTokenID {
+			t.Errorf("Worng apiToken. test set: %+v", c)
+		}
+		if client.ApiSecret != c.expect.client.ApiSecret {
+			t.Errorf("Worng ApiSecret. test set: %+v", c)
+		}
+		if reflect.TypeOf(client.HTTPClient) != reflect.TypeOf(c.expect.client.HTTPClient) {
+			t.Errorf("Worng HTTPClient. test set: %+v", c)
+		}
+		if reflect.TypeOf(client.Logger) != reflect.TypeOf(c.expect.client.Logger) {
+			t.Errorf("Worng Logger. test set: %+v", c)
+		}
 	}
 }
+func TestNewRequest(t *testing.T) {
+	type Param struct {
+		method     string
+		spath      string
+		queryParam *map[string]string
+	}
+	type Expect struct {
+		method string
+		url    string
+	}
+	cases := []struct {
+		param  Param
+		expect Expect
+	}{
+		// test case 1
+		{
+			param:  Param{method: "GET", spath: "product/1", queryParam: nil},
+			expect: Expect{method: "GET", url: "https://api.quoine.com/product/1"},
+		},
+		// test case 2
+		{
+			param:  Param{method: "GET", spath: "product/1", queryParam: &map[string]string{"product_id": "1", "limit": "1", "page": "1"}},
+			expect: Expect{method: "GET", url: "https://api.quoine.com/product/1?limit=1&page=1&product_id=1"},
+		},
+	}
 
-func TestNewClientApiSecretError(t *testing.T) {
-	if _, e := NewClient("apiToken", "", nil); e == nil {
-		t.Error("err should be returned")
-	}
-}
-
-func TestNewClientSuccess(t *testing.T) {
-	c, e := NewClient("apiTokenID", "secret", nil)
-	if e != nil {
-		t.Error("err should be nil")
-	}
-	if c.ApiTokenID != "apiTokenID" {
-		t.Error("Worng apiToken")
-	}
-	if c.ApiSecret != "secret" {
-		t.Error("Worng ApiSecret")
-	}
-	if c.HTTPClient == nil {
-		t.Error("HttpClient is nil")
-	}
-	if c.Logger == nil {
-		t.Error("Logger is nil")
-	}
-}
-
-func TestNewRequestSuccessNoQueryParam(t *testing.T) {
-	c, _ := NewClient("apiTokenID", "secret", nil)
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	req, err := c.newRequest(ctx, "GET", "product/1", nil, nil)
-	if err != nil {
-		t.Error("Error is occered")
-	}
-	if req.Method != "GET" {
-		t.Error("Worng method")
-	}
-	if len(req.Header["X-Quoine-Auth"]) < 1 {
-		t.Error("Worng Header")
-	}
-	if req.URL.String() != "https://api.quoine.com/product/1" {
-		t.Errorf("Worng URL : %+v", req.URL.String())
-	}
-}
-
-func TestNewRequestSuccessWithQueryParam(t *testing.T) {
-	c, _ := NewClient("apiTokenID", "secret", nil)
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	queryParam := &map[string]string{"product_id": "1", "limit": "1", "page": "1"}
-	req, err := c.newRequest(ctx, "GET", "product/1", nil, queryParam)
-	if err != nil {
-		t.Error("Error is occered")
-	}
-	if req.Method != "GET" {
-		t.Error("Worng method")
-	}
-	if len(req.Header["X-Quoine-Auth"]) < 1 {
-		t.Error("Worng Header")
-	}
-	aq := req.URL.Query()
-	for k, v := range *queryParam {
-		if aq[k][0] != v {
-			t.Errorf("Worng Query Parameter k:%+v, v:%+v", k, v)
+	for _, c := range cases {
+		client, _ := NewClient("apiTokenID", "secret", nil)
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		req, _ := client.newRequest(ctx, c.param.method, c.param.spath, nil, c.param.queryParam)
+		if req.Method != c.expect.method {
+			t.Errorf("Worng method. case: %+v", c)
+		}
+		if len(req.Header["X-Quoine-Auth"]) < 1 {
+			t.Errorf("Worng Header. case: %+v", c)
+		}
+		if req.URL.String() != c.expect.url {
+			t.Errorf("Worng URL case: %+v, actual: %+v", c, req.URL.String())
 		}
 	}
 }
 
 func TestGetAnOrder(t *testing.T) {
-	// preparing test server
-	ts := httptest.NewServer(http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			if r.URL.Path != "/orders/1" {
-				t.Fatalf("worng URL")
-			}
-			//if r.URL.Query().Get("greet") != "Hello" {
-			//	t.Fatalf("worng Query Params")
-			//}
-			// set expected json
-			w.Header().Set("content-Type", "text")
-			fmt.Fprintf(w, getOrderJsonResponse())
-			return
+	type Param struct {
+		orderID      int
+		jsonResponse string
+	}
+	type Expect struct {
+		path  string
+		order *models.Order
+	}
+	cases := []struct {
+		param  Param
+		expect Expect
+	}{
+		// test case 1
+		{
+			param: Param{orderID: 1, jsonResponse: getOrderJsonResponse()},
+			expect: Expect{path: "/orders/1", order: &models.Order{
+				ID:                   2157479,
+				OrderType:            "limit",
+				Quantity:             "0.01",
+				DiscQuantity:         "0.0",
+				IcebergTotalQuantity: "0.0",
+				Side:                 "sell",
+				FilledQuantity:       "0.01",
+				Price:                "500.0",
+				CreatedAt:            1462123639,
+				UpdatedAt:            1462123639,
+				Status:               "filled",
+				LeverageLevel:        2,
+				SourceExchange:       "QUOINE",
+				ProductID:            1,
+				ProductCode:          "CASH",
+				FundingCurrency:      "USD",
+				CurrencyPairCode:     "BTCUSD",
+				OrderFee:             "0.0",
+				Executions: models.OrderExecutions{
+					{
+						ID:        4566133,
+						Quantity:  "0.01",
+						Price:     "500.0",
+						TakerSide: "buy",
+						MySide:    "sell",
+						CreatedAt: 1465396785,
+					},
+				},
+			}},
 		},
-	))
-	defer ts.Close()
+		// test case 2
+	}
+	for _, c := range cases {
+		// preparing test server
+		ts := httptest.NewServer(http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path != c.expect.path {
+					t.Fatalf("worng URL")
+				}
+				// set expected json
+				w.Header().Set("content-Type", "text")
+				fmt.Fprintf(w, c.param.jsonResponse)
+				return
+			},
+		))
+		defer ts.Close()
 
-	c, _ := NewClient("apiTokenID", "secret", nil)
-	c.testServer = ts
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	order, err := c.GetAnOrder(ctx, 1)
-	if err != nil {
-		t.Errorf("Error is not nil. Order : %+v, Error : %+v", order, err)
-	}
-	if order.ID != 2157479 {
-		t.Errorf("Worng ID. Order : %+v,", order)
-	}
-	if order.OrderType != "limit" {
-		t.Errorf("Worng OrderType. Order : %+v,", order)
-	}
-	if order.Quantity != "0.01" {
-		t.Errorf("Worng Quantity. Order : %+v,", order)
-	}
-	if order.DiscQuantity != "0.0" {
-		t.Errorf("Worng DiscQuantity. Order : %+v,", order)
-	}
-	if order.IcebergTotalQuantity != "0.0" {
-		t.Errorf("Worng IcebergTotalQuantity. Order : %+v,", order)
-	}
-	if order.Side != "sell" {
-		t.Errorf("Worng Side. Order : %+v,", order)
-	}
-	if order.FilledQuantity != "0.01" {
-		t.Errorf("Worng FilledQuantity. Order : %+v,", order)
-	}
-	if order.Price != "500.0" {
-		t.Errorf("Worng Price. Order : %+v,", order)
-	}
-	if order.CreatedAt != 1462123639 {
-		t.Errorf("Worng CreatedAt. Order : %+v,", order)
-	}
-	if order.UpdatedAt != 1462123639 {
-		t.Errorf("Worng UpdatedAt. Order : %+v,", order)
-	}
-	if order.Status != "filled" {
-		t.Errorf("Worng Status. Order : %+v,", order)
-	}
-	if order.LeverageLevel != 2 {
-		t.Errorf("Worng LeverageLevel. Order : %+v,", order)
-	}
-	if order.SourceExchange != "QUOINE" {
-		t.Errorf("Worng SourceExchange. Order : %+v,", order)
-	}
-	if order.ProductID != 1 {
-		t.Errorf("Worng ProductID. Order : %+v,", order)
-	}
-	if order.ProductCode != "CASH" {
-		t.Errorf("Worng ProductCode. Order : %+v,", order)
-	}
-	if order.ProductCode != "CASH" {
-		t.Errorf("Worng ProductCode. Order : %+v,", order)
-	}
-	if order.FundingCurrency != "USD" {
-		t.Errorf("Worng FundingCurrency. Order : %+v,", order)
-	}
-	if order.CurrencyPairCode != "BTCUSD" {
-		t.Errorf("Worng CurrencyPairCode. Order : %+v,", order)
-	}
-	if order.OrderFee != "0.0" {
-		t.Errorf("Worng OrderFee. Order : %+v,", order)
-	}
-	if order.Executions[0].ID != 4566133 {
-		t.Errorf("Worng Executions.ID. Order : %+v,", order)
-	}
-	if order.Executions[0].Quantity != "0.01" {
-		t.Errorf("Worng Executions.Quantity. Order : %+v,", order)
-	}
-	if order.Executions[0].Price != "500.0" {
-		t.Errorf("Worng Executions.Price. Order : %+v,", order)
-	}
-	if order.Executions[0].TakerSide != "buy" {
-		t.Errorf("Worng Executions.TakerSide. Order : %+v,", order)
-	}
-	if order.Executions[0].MySide != "sell" {
-		t.Errorf("Worng Executions.MySide. Order : %+v,", order)
-	}
-	if order.Executions[0].CreatedAt != 1465396785 {
-		t.Errorf("Worng Executions.CreatedAt. Order : %+v,", order)
+		client, _ := NewClient("apiTokenID", "secret", nil)
+		client.testServer = ts
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		order, _ := client.GetAnOrder(ctx, 1)
+		if !cmp.Equal(order, c.expect.order) {
+			t.Errorf("Worng attribute. %+v", cmp.Diff(order, c.expect.order))
+
+		}
+
 	}
 }
 
@@ -214,3 +222,57 @@ func getOrderJsonResponse() string {
   	]
 	}`
 }
+
+//func TestGetProducts(t *testing.T) {
+//	// preparing test server
+//	ts := httptest.NewServer(http.HandlerFunc(
+//		func(w http.ResponseWriter, r *http.Request) {
+//			if r.URL.Path != "/products" {
+//				t.Fatalf("worng URL")
+//			}
+//			w.Header().Set("content-Type", "text")
+//			fmt.Fprintf(w, getProductsJsonResponse())
+//			return
+//		},
+//	))
+//	defer ts.Close()
+//
+//	c, _ := NewClient("apiTokenID", "secret", nil)
+//	c.testServer = ts
+//	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+//	products, err := c.GetProducts(ctx)
+//	t.Logf("products: %v", products)
+//	if err != nil {
+//		t.Errorf("Worng err is not nil. Error: %v,", err)
+//	}
+//}
+
+//func getProductsJsonResponse() string {
+//	return `[
+//    {
+//        "id": 5,
+//        "product_type": "CurrencyPair",
+//        "code": "CASH",
+//        "name": "CASH Trading",
+//        "market_ask": "48203.05",
+//        "market_bid": "48188.15",
+//        "indicator": -1,
+//        "currency": "JPY",
+//        "currency_pair_code": "BTCJPY",
+//        "symbol": "¥",
+//        "fiat_minimum_withdraw": "1500.0",
+//        "pusher_channel": "product_cash_btcjpy_5",
+//        "taker_fee": "0.0",
+//        "maker_fee": "0.0",
+//        "low_market_bid": "47630.99",
+//        "high_market_ask": "48396.71",
+//        "volume_24h": "2915.627366519999999998",
+//        "last_price_24h": "48217.2",
+//        "last_traded_price": "48203.05",
+//        "last_traded_quantity": "1.0",
+//        "quoted_currency": "JPY",
+//        "base_currency": "BTC",
+//        "exchange_rate": "0.009398151671149725"
+//    },
+//  ]`
+//}
--- v2/models/order.go
+++ v2/models/order.go
@@ -1,30 +1,32 @@
 package models
 
 type Order struct {
-	ID                   int    `json:"id"`
-	OrderType            string `json:"order_type"`
-	Quantity             string `json:"quantity"`
-	DiscQuantity         string `json:"disc_quantity"`
-	IcebergTotalQuantity string `json:"iceberg_total_quantity"`
-	Side                 string `json:"side"`
-	FilledQuantity       string `json:"filled_quantity"`
-	Price                string `json:"price"`
-	CreatedAt            int    `json:"created_at"`
-	UpdatedAt            int    `json:"updated_at"`
-	Status               string `json:"status"`
-	LeverageLevel        int    `json:"leverage_level"`
-	SourceExchange       string `json:"source_exchange"`
-	ProductID            int    `json:"product_id"`
-	ProductCode          string `json:"product_code"`
-	FundingCurrency      string `json:"funding_currency"`
-	CurrencyPairCode     string `json:"currency_pair_code"`
-	OrderFee             string `json:"order_fee"`
-	Executions           []struct {
-		ID        int    `json:"id"`
-		Quantity  string `json:"quantity"`
-		Price     string `json:"price"`
-		TakerSide string `json:"taker_side"`
-		MySide    string `json:"my_side"`
-		CreatedAt int    `json:"created_at"`
-	} `json:"executions"`
+	ID                   int             `json:"id"`
+	OrderType            string          `json:"order_type"`
+	Quantity             string          `json:"quantity"`
+	DiscQuantity         string          `json:"disc_quantity"`
+	IcebergTotalQuantity string          `json:"iceberg_total_quantity"`
+	Side                 string          `json:"side"`
+	FilledQuantity       string          `json:"filled_quantity"`
+	Price                string          `json:"price"`
+	CreatedAt            int             `json:"created_at"`
+	UpdatedAt            int             `json:"updated_at"`
+	Status               string          `json:"status"`
+	LeverageLevel        int             `json:"leverage_level"`
+	SourceExchange       string          `json:"source_exchange"`
+	ProductID            int             `json:"product_id"`
+	ProductCode          string          `json:"product_code"`
+	FundingCurrency      string          `json:"funding_currency"`
+	CurrencyPairCode     string          `json:"currency_pair_code"`
+	OrderFee             string          `json:"order_fee"`
+	Executions           OrderExecutions `json:"executions"`
+}
+
+type OrderExecutions []struct {
+	ID        int    `json:"id"`
+	Quantity  string `json:"quantity"`
+	Price     string `json:"price"`
+	TakerSide string `json:"taker_side"`
+	MySide    string `json:"my_side"`
+	CreatedAt int    `json:"created_at"`
 }
